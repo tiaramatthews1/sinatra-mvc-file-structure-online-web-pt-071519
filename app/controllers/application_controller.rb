bfef6de872e0c084ca0a80003d9722b36546ff7d
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-10-01 17:10:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,19 @@ class ApplicationController < Sinatra::Base
   end
 end
 
+class Dog
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name, breed, age)
+    self.name = name
+    self.breed = breed
+    self.age = age
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
